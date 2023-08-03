9cc7b0adb3ad2acb2912ae84c82355d9e4f65257
ALADESIUN IDOWU ADEDAMOLA <highdee.ai@gmail.com>
2023-08-03 23:28:25 +0100

    Update upgrader.dart

--- lib/src/upgrader.dart
+++ lib/src/upgrader.dart
@@ -415,7 +415,7 @@ class Upgrader {
       }
       if (shouldDisplay) {
         _displayed = true;
-        Future.delayed(const Duration(seconds: 8), () {
+        Future.delayed(const Duration(seconds: 10), () {
           _showDialog(
               context: context,
               title: messages.message(UpgraderMessage.title),
